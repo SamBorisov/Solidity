b7c714990d3fa6882100b7345cd882f9e8486a6a
Samuil Borisov <88675952+SamBorisov@users.noreply.github.com>
2023-11-20 16:26:00 +0200

    Create ProjectsCustomDecimalsToken.sol

---
+++ Projects/ProjectsCustomDecimalsToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+
+    uint8 private immutable tokenDecimals;
+
+    constructor(address owner, uint amount,uint8 decimals_) ERC20("My Mock Token", "MMT") {
+        tokenDecimals = decimals_; 
+        uint256 initialSupply = amount * 10 ** decimals_; 
+        _mint(owner, initialSupply);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return tokenDecimals; 
+    }
+}
